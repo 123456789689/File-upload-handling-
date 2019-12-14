654786f101f650e4f6073b3688d5dac04f4ec98a
Mourya S <58628352+123456789689@users.noreply.github.com>
2019-12-14 18:35:56 +0530

    Update FileuploadHandle.java

--- Fileupload/src/main/java/HandleFileuploadPopup/Fileupload/FileuploadHandle.java
+++ Fileupload/src/main/java/HandleFileuploadPopup/Fileupload/FileuploadHandle.java
@@ -18,6 +18,13 @@ public class FileuploadHandle {
 
 		d.findElement(By.xpath("//input[@id='fileupload']")).sendKeys("C:\\Users\\mourya.s\\Desktop\\download LLR.pdf");
 		//While uploading file should not use click() method, always use .sendkeys method
+		
+		//While uploading file should not use click() method, always use .sendkeys method. Also use .sendkeys method only if type=file is there when u inspect in HTML 
+		
+		//3 Types of popup is there
+		//1. alerts -- javascript popup--Alert API (accept, dismiss)
+		//2. File upload pop up--Browse btn (type = file, .sendkeys)
+		//3. Browser window popup -- Advertisement popup (windowhandler API - .getWindowHandles() )
 
 	}
 
